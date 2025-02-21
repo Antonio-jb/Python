06d8b17c98dafa4c3c76c16d99089b770d54b93e
antonio <antonio_ajb@outlook.com>
2025-02-21 13:14:07 +0100

    Blackjack

---
+++ blackjack/Ventiuna.py
@@ -0,0 +1,26 @@
+# baraja = [2,3,4]
+# manoCasa = []
+# manoJugador = []
+#
+# print (baraja)
+# carta = baraja.pop()
+# print(baraja)
+# print(carta)
+
+# Ojetivo: Llegar a 21 sin pasarse
+# Juegas contra la casa
+# Te puedes plantar siempre que no hayas llegado
+# o seguir pidiendo cartas.
+# La casa se planta cuando tiene 17 o mas cartas
+# A = 1, 11
+# J, Q, K = 10
+# 2..10 = su valor
+
+from blackjack.Carta import Carta
+
+baraja = []
+baraja.append(Carta("♠", 3, f"{3} de ♠"))
+
+print(carta)
+
+print(carta.valor)
