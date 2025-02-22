e3ca8ac91110a28fde75e149c1f684fe98475a3f
Antonio <antonio_ajb@outlook.com>
2025-02-22 16:01:57 +0100

    Blackjack Hecho

--- blackjack/Carta.py
+++ blackjack/Carta.py
@@ -1,10 +1,8 @@
 class Carta:
-    def __init__(self, palo, valor, nombre):
-        self.palo = palo
+    def __init__(self, valor, palo, nombre):
         self.valor = valor
+        self.palo = palo
         self.nombre = nombre
 
-    def __str__(self):
+    def __repr__(self):
         return f"{self.nombre}"
-
-
--- blackjack/Ventiuna.py
+++ blackjack/Ventiuna.py
@@ -17,10 +17,121 @@
 # 2..10 = su valor
 
 from blackjack.Carta import Carta
+import random
+import time
 
 baraja = []
-baraja.append(Carta("♠", 3, f"{3} de ♠"))
+palos = ["♣️", "♠️", "♥️", "♦️"]
+letras = ["J", "Q", "K", "A"]
 
-print(carta)
+barajaCasa = []
+barajaJugador = []
 
-print(carta.valor)
+def baraja_nueva():
+    for palo in palos:
+        for i in range(2, 11):
+            baraja.append(Carta(i, palo, f"{i} de {palo}"))
+
+        for letra in letras:
+            baraja.append(Carta(letra, palo, f"{letra} de {palo}"))
+
+def barajear(baraja):
+    random.shuffle(baraja)
+
+
+def valor_cartas(baraja):
+    suma_baraja = 0
+    for carta in baraja:
+        valor_carta = carta.valor
+
+        if valor_carta in ["K", "J", "Q"]:
+            valor_carta = 10
+
+        elif valor_carta == "A":
+            if suma_baraja + 11 > 21:
+                valor_carta = 1
+            else:
+                valor_carta = 11
+
+        elif isinstance(valor_carta, int):
+            valor_carta = carta.valor
+
+        suma_baraja += valor_carta
+
+    return suma_baraja
+
+
+def repartir_carta(baraja, to_baraja):
+    nombre = ""
+    if to_baraja == barajaCasa:
+        nombre = "cupier"
+    if to_baraja == barajaJugador:
+        nombre = "jugador"
+    print(f"repartiendo carta a {nombre}...")
+    print("--------------------------------------------")
+    time.sleep(3)
+    carta_sacada = baraja.pop()
+    to_baraja.append(carta_sacada)
+    print(f"LA CARTA ES -> {carta_sacada.nombre}")
+    print("-----------------------------------------")
+    print(f"La suma de la baraja del {nombre_jugador(to_baraja)} es: {valor_cartas(to_baraja)}")
+    print("-----------------------------------------")
+
+def nombre_jugador(baraja):
+    nombre = ""
+    if baraja == barajaCasa:
+        nombre = "cupier"
+    if baraja == barajaJugador:
+        nombre = "jugador"
+
+    return nombre
+
+def inicio_partida():
+    for i in range (0, 2):
+        repartir_carta(baraja, barajaJugador)
+
+def comprobar_cartas():
+    if valor_cartas(barajaJugador) > 21:
+        print("EL CUPIER HA GANADO")
+        exit(0)
+    if valor_cartas(barajaCasa) > 21:
+        print("EL JUGADOR HA GANADO")
+        exit(0)
+    if valor_cartas(barajaJugador) == 21:
+        print("EL JUGADOR HA GANADO")
+        exit(0)
+    if valor_cartas(barajaCasa) == 21:
+        print("EL CUPIER HA GANADO")
+        exit(0)
+
+print("--------------------------------------------")
+print("Empezando partida...")
+print("--------------------------------------------")
+baraja_nueva()
+barajear(baraja)
+
+inicio_partida()
+repartir_carta(baraja, barajaCasa)
+
+plantarse_respuesta = ""
+plantarse = False
+while valor_cartas(barajaJugador) < 21 and plantarse == False:
+    plantarse_respuesta = input("¿Te quieres plantar? (S/N)")
+    if plantarse_respuesta.lower() == "n":
+        repartir_carta(baraja, barajaJugador)
+        comprobar_cartas()
+    else:
+        plantarse = True
+        print("")
+        print("--------------------------------------------")
+        print("El jugador se ha plantado")
+        print("--------------------------------------------")
+        print("")
+        time.sleep(1)
+
+if valor_cartas(barajaCasa) >= 17:
+    print("El cupier se ha plantado")
+else:
+    while valor_cartas(barajaCasa) < 17 or valor_cartas(barajaJugador) == 21:
+        repartir_carta(baraja, barajaCasa)
+        comprobar_cartas()
